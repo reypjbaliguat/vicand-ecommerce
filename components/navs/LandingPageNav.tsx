7f4c9079b506ce46634809e0d3cdd451d548f904
reypjbaliguat <reypjbaliguat@gmail.com>
2024-02-07 20:17:26 +0800

    nav update

--- components/navs/LandingPageNav.tsx
+++ components/navs/LandingPageNav.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import logo from "@/public/assets/vicand-logo.svg";
 import Image from "next/image";
 import { navMenus } from "../constants/navMenus";
 import Link from "next/link";
 import Icon from "@mdi/react";
 import { mdiAccountCircle, mdiMagnify, mdiShoppingOutline } from "@mdi/js";
+import { IconButton } from "@mui/material";
+import { usePathname } from "next/navigation";
 
 function LandingPageNav() {
+  const pathname = usePathname();
+  console.log({ pathname });
   return (
     <nav className="flex items-center justify-between h-20 px-24">
       <Link href="/">
@@ -14,16 +20,28 @@ function LandingPageNav() {
       <div className="flex items-center gap-10">
         {navMenus.map((menu) => {
           return (
-            <Link href={menu.path} key={menu.path}>
+            <Link
+              href={menu.path}
+              key={menu.path}
+              className={`hover:underline underline-offset-4 decoration-2 ${
+                pathname === menu.path && "underline"
+              }`}
+            >
               {menu.label}
             </Link>
           );
         })}
       </div>
       <div className="flex gap-5">
-        <Icon path={mdiMagnify} size={1} />
-        <Icon path={mdiAccountCircle} size={1} />
-        <Icon path={mdiShoppingOutline} size={1} />
+        <IconButton>
+          <Icon path={mdiMagnify} size={1} className="text-black" />
+        </IconButton>
+        <IconButton>
+          <Icon path={mdiAccountCircle} size={1} className="text-black" />
+        </IconButton>
+        <IconButton>
+          <Icon path={mdiShoppingOutline} size={1} className="text-black" />
+        </IconButton>
       </div>
     </nav>
   );
